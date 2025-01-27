15c580c6a791f73d5c79ac79db2dd94a0e68a879
satstracker <at.anoop@outlook.com>
2025-01-27 18:53:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind1
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 // Function to fetch Bitcoin price and related data\
+// Function to fetch Bitcoin price and related data\
 async function fetchBitcoinData() \{\
   try \{\
     const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,cad,inr,eur');\
